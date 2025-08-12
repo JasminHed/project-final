85654679fcb440b67fcb4c2d3a46e1d816add209
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-08-12 19:07:54 +0200

    added min length charc in setup for goal

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -7,12 +7,12 @@ const goalSchema = new mongoose.Schema({
     required: true 
   },
 
-  intention: { type: String, required: true, maxlength: 150 },
-  specific: { type: String, required: true, maxlength: 150 },
-  measurable: { type: String, required: true, maxlength: 150 },
-  achievable: { type: String, required: true, maxlength: 150 },
-  relevant: { type: String, required: true, maxlength: 150 },
-  timebound: { type: String, required: true, maxlength: 150 },
+  intention: { type: String, required: true, minlength: 20, maxlength: 150  },
+  specific: { type: String, required: true, minlength: 20, maxlength: 150 },
+  measurable: { type: String, required: true, minlength: 20, maxlength: 150 },
+  achievable: { type: String, required: true, minlength: 20, maxlength: 150 },
+  relevant: { type: String, required: true, minlength: 20, maxlength: 150 },
+  timebound: { type: String, required: true, minlength: 20, maxlength: 150 },
   completed: { type: Boolean, default: false },
   isPublic: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }

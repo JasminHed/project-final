042d22f511d8dd846ee4fa15149a248f80ccfa40
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-11-14 10:04:58 +0100

    gmail transf

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,10 @@ import User from "./models/User.js";
 
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: "smtp.sendgrid.net",
-  port: 587,
+  service: "gmail",
   auth: {
-    user: "apikey",
-    pass: process.env.SENDGRID_API_KEY,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
